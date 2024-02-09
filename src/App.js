0d903a3ad18b6284a0509c6e5ee9ebefa9386986
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-09 15:56:12 -0300

    Capítulo 01: Uma Biblioteca Declarativa - Lição 04: Primeiro componente: Banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
+        <Banner></Banner>
         <a
           className="App-link"
           href="https://reactjs.org"

04f84058a63bd9aa7c7b773dc9fbd5c0fd66db00
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-10 08:39:29 -0300

    Capítulo 01: Uma Biblioteca Declarativa - Lição 08: Criando o formulário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from "./components/Banner/Banner";
-import TextInput from "./components/TextInput";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
         <Banner/>
-        <TextInput label="Nome" placeholder="Digite seu nome"/>
-        <TextInput label="Cargo" placeholder="Digite seu cargo"/>
-        <TextInput label="Imagem" placeholder="Digite o endereço da imagem"/>
+        <Form/>
     </div>
   );
 }

d441c1ee8fa15538b5650d17eeb658dbcaa7b666
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-10 07:35:46 -0300

    Capítulo 01: Uma Biblioteca Declarativa - Lição 06: Campo de Texto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from "./components/Banner/Banner";
+import TextInput from "./components/TextInput";
 
 function App() {
   return (
     <div className="App">
         <Banner/>
+        <TextInput></TextInput>
     </div>
   );
 }
---
+++ src/components/TextInput/index.js
@@ -0,0 +1,12 @@
+import './TextInput.css';
+
+const TextInput = () => {
+    return (
+        <div className="text-input">
+                <label>Nome</label>
+                <input placeholder="Digite seu nome"/>
+        </div>
+    )
+}
+
+export default TextInput;

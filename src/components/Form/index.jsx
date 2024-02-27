d6f9dfb861e8379e349c7a9318e17c2ad150b449
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-27 03:19:15 -0300

    Capítulo 02: Trabalhando com props - Lição 05: Escutando eventos

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -14,12 +14,17 @@ const Form = () => {
         'Inovação e Gestão'
     ]
 
+    const onSave = (event) => {
+        event.preventDefault();        
+        console.log('Form was submited');
+    }
+
     return (
         <section className="form-employees">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextInput label="Nome" placeholder="Digite seu nome"/>
-                <TextInput label="Cargo" placeholder="Digite seu cargo"/>
+                <TextInput required={true} label="Nome" placeholder="Digite seu nome"/>
+                <TextInput required={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <TextInput label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <DropDown label="Times" itens={times}></DropDown>
                 <Button>Criar Card</Button>

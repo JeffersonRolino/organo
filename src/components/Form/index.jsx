ea5507b7bfed60da6bfe8c642184743585018fc7
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-27 01:21:18 -0300

    Capítulo 02: Trabalhando com props - Lição 03: Lista dos times

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,7 +1,18 @@
+import DropDown from "../DropDown";
 import TextInput from "../TextInput";
 import './Form.css';
 
 const Form = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className="form-employees">
             <form>
@@ -9,6 +20,7 @@ const Form = () => {
                 <TextInput label="Nome" placeholder="Digite seu nome"/>
                 <TextInput label="Cargo" placeholder="Digite seu cargo"/>
                 <TextInput label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <DropDown label="Times" itens={times}></DropDown>
             </form>
         </section>
     )

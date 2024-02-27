a60e022ac26a25a38acc2040e126b920adc4af14
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-27 07:55:03 -0300

    Capítulo 03: Interagindo com o usuário - Lição 02: Manipulando o input do usuário

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,10 +1,18 @@
 import './TextInput.css';
 
 const TextInput = (props) => {
+
+    let inputValue = 'Teste';
+
+    const onTyped = (event) => {
+        inputValue = event.target.value;
+        console.log(event.target.value);
+    }
+
     return (
         <div className="text-input">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={props.placeholder}/>
+            <input value={inputValue} onChange={onTyped} required={props.required} placeholder={props.placeholder}/>
         </div>
     )
 }

89be881d1deb88011f0a89c660c2a00444ffc4e9
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-01 06:46:20 -0300

    Capítulo 04: Montando Times - Lição 02: Criando o componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./components/Banner/Banner";
 import Form from "./components/Form";
+import Team from "./components/Team";
 
 function App() {
     const [employes, setEmployes] = useState([]);
@@ -14,6 +15,9 @@ function App() {
         <div className="App">
             <Banner />
             <Form registeredEmploye={(employe) => newEmploye(employe)} />
+            <Team name="Programação" />
+            <Team name="Frontend" />
+            <Team name="Data Science" />
         </div>
     );
 }
